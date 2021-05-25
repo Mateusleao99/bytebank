a7a276001700d77e585f429e00e7795ddff0afea
Mateus Leao <mateus.san.tos@hotmail.com>
2021-05-25 08:48:43 -0300

    feat: criando component extrato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  transferencia:any;
+  transferencias:any[] = [];
 
 
   transferir($event){
     console.log($event);
-    this.transferencia = $event;
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
   }
 }
